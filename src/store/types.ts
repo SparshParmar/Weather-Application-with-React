43aa85fb33407f5f72a4ac2774bf5eae14e758c4
sparsh_parmar <sparshparmar444@gmail.com>
2020-08-04 15:27:14 +0200

    feat: add weatherActions

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,4 +1,4 @@
-export const GET_WEATEHER = 'GET_WEATEHER';
+export const GET_WEATHER = 'GET_WEATHER';
 export const SET_LOADING = 'SET_LOADING';
 export const SET_ERROR = 'SET_ERROR';
 export const SET_ALERT = 'SET_ALERT';
@@ -10,7 +10,7 @@ export interface Weather {
   main: string;
 }
 
-export interface WeaatherData {
+export interface WeatherData {
   base: string;
   clouds: {
     all: number;
